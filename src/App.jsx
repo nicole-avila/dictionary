cefc224ce3426acb78654e2c0c3ce50a42d2bf54
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-09-19 18:49:53 +0200

    creating app and adjus. sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import "./App.scss";
+import SearchBar from "./components/SearchBar/SearchBar";
+import DisplaySearchWord from "./components/DisplaySearchWord/DisplaySearchWord";
+import { FavoriteListProvider } from "./components/FavoriteListContext/FavoriteListContext";
+import FavoriteList from "./components/FavoriteList/FavoriteList";
 
 function App() {
+  const [searchWord, setSearchWord] = useState("");
+  const [favoriteStar, setFavoriteStar] = useState(false);
+
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <FavoriteListProvider>
+      <div className="app">
+        <SearchBar
+          searchWord={searchWord}
+          setSearchWord={setSearchWord}
+          setFavoriteStar={setFavoriteStar}
+        />
+
+        {Array.isArray(searchWord) && searchWord.length > 0 && (
+          <DisplaySearchWord
+            searchWord={searchWord}
+            favoriteStar={favoriteStar}
+            setFavoriteStar={setFavoriteStar}
+          />
+        )}
+      </div>
+      <FavoriteList />
+    </FavoriteListProvider>
   );
 }
 
