736446a8e02d970428441fd98031d1ff63b4983d
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-09-25 00:01:16 +0200

    icon in darkMode toggle done + Header Comp

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,8 +13,11 @@ export default function Header({ setFavoriteListVisible }) {
         <DarkThemeButton />
       </div>
       <div className="header__container" onClick={toggleFavoriteList}>
-        <h1 className="header__title">dictionary</h1>
         <div>
+          <h1 className="header__title">dictionary</h1>
+          <p className="header__p">[dik-shuh-ner-ee]</p>
+        </div>
+        <div className="header__heart-container">
           <img
             className="header__heart-icon"
             src={favoriteHeart}
